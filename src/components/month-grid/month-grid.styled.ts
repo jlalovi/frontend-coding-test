596a204cc4f88ed78968280646a0ffbedb3ae244
Jaime Sarrión <66952690+jsarrion-RatedPower@users.noreply.github.com>
2022-03-03 18:29:25 +0100

    fix: month container grid

--- src/components/month-grid/month-grid.styled.ts
+++ src/components/month-grid/month-grid.styled.ts
@@ -3,9 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr;
+  grid-template-rows: 140px 1fr;
   border-top: 1px solid ${({ theme }) => theme.gray_300};
   border-left: 1px solid ${({ theme }) => theme.gray_300};
 `;
+
 export const Week = styled.div`
   display: grid;
   grid-template-columns: repeat(7, 1fr);
@@ -13,23 +15,32 @@ export const Week = styled.div`
 
 export const TopBar = styled.div`
   display: grid;
-  grid-template-columns: 100px 1fr 100px;
   width: 100%;
-  height: 0;
+  grid-template-columns: 200px 200px;
+  justify-content: space-between;
+`;
 
-  .button-group {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-column-gap: 1em;
-    color: ${({ theme }) => theme.cyan_600};
-    padding: 2rem 0;
+export const MonthContainer = styled.div`
+  display: grid;
+`;
 
-    svg {
-      cursor: pointer;
-    }
+export const ButtonGroup = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 1em;
+  color: ${({ theme }) => theme.cyan_600};
+  padding: 2rem 1rem;
+  justify-items: center;
+  align-content: center;
+
+  svg {
+    cursor: pointer;
   }
-  .title {
-    padding: 2rem;
-    font-size: 40px;
-  }
+`;
+
+export const Title = styled.div`
+  padding: 2rem;
+  font-size: 40px;
+  justify-self: center;
+  align-self: center;
 `;
--- src/components/month-grid/month-grid.tsx
+++ src/components/month-grid/month-grid.tsx
@@ -13,9 +13,8 @@ const MonthGrid: React.FC = () => {
   return (
     <Styles.Container>
       <Styles.TopBar>
-        <div className="title">{MONTH_NAMES[currentMonth]}</div>
-        <div />
-        <div className="button-group">
+        <Styles.Title>{MONTH_NAMES[currentMonth]}</Styles.Title>
+        <Styles.ButtonGroup>
           <ArrowLeftCircle
             size={40}
             onClick={() =>
@@ -28,23 +27,25 @@ const MonthGrid: React.FC = () => {
               currentMonth < 11 && setCurrentMonth(currentMonth + 1)
             }
           />
-        </div>
+        </Styles.ButtonGroup>
       </Styles.TopBar>
-      {currentCalendar.map(({ days }, weekIndex) => (
-        <Styles.Week key={weekIndex}>
-          {days.map((day, key) => (
-            <DayGridItem
-              key={key}
-              {...{
-                day,
-                disabled:
-                  (weekIndex === 0 && day > 25) ||
-                  (weekIndex === currentCalendar.length - 1 && day < 22),
-              }}
-            />
-          ))}
-        </Styles.Week>
-      ))}
+      <Styles.MonthContainer>
+        {currentCalendar.map(({ days }, weekIndex) => (
+          <Styles.Week key={weekIndex}>
+            {days.map((day, key) => (
+              <DayGridItem
+                key={key}
+                {...{
+                  day,
+                  disabled:
+                    (weekIndex === 0 && day > 25) ||
+                    (weekIndex === currentCalendar.length - 1 && day < 22),
+                }}
+              />
+            ))}
+          </Styles.Week>
+        ))}
+      </Styles.MonthContainer>
     </Styles.Container>
   );
 };
